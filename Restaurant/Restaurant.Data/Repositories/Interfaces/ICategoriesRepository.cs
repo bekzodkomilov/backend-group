7407f8604c876491ab1dba2f5371f42a5de00de7
bekzodkomilov <komilovbek1998@gmail.com>
2022-06-21 00:13:39 +0500

    Categories Logic

---
+++ Restaurant/Restaurant.Data/Repositories/Interfaces/ICategoriesRepository.cs
@@ -0,0 +1,14 @@
+using Restaurant.Domain.Entities.ApiEntities;
+
+namespace Restaurant.Data.Repositories.Interfaces;
+
+public interface ICategoriesRepository
+{
+    Task<Category> GetByIdAsync(Guid id);
+    Task<List<Category>> GetByCategoryIdAsync(Guid id);
+    Task<List<Category>> GetAllAsync(Func<Category, bool> p = null);
+    Task<(bool IsSuccess, Exception e)> InsertAsync(Category category);
+    Task<(bool IsSuccess, Exception e)> UpdateAsync(Category category);
+    Task<(bool IsSuccess, Exception e)> DeleteIdAsync(Guid id);
+
+}
