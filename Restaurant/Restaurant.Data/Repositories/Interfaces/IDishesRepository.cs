359f37ce02bd4238b91aa703f7e9462fcd3daa12
Jam0l <xjamoliddin27@gmail.com>
2022-06-21 18:23:52 +0500

    updated issue7

---
+++ Restaurant/Restaurant.Data/Repositories/Interfaces/IDishesRepository.cs
@@ -0,0 +1,12 @@
+using Restaurant.Domain.Entities.ApiEntities;
+
+namespace Restaurant.Data.Repositories.Interfaces;
+public interface IDishesRepository
+{
+    Task<Dish> GetByIdAsync(Guid id);
+    Task<List<Dish>> GetAllAsync(Func<Dish, bool> d = null);
+    Task<List<Dish>> GetByCategoryIdAsync(Guid id);
+    Task<(bool IsSuccess, Exception e)> InsertAsync(Dish dish);
+    Task<(bool IsSuccess, Exception e)> UpdateAsync(Dish dish);
+    Task<(bool IsSuccess, Exception e)> DeleteIdAsync(Guid id);
+}
